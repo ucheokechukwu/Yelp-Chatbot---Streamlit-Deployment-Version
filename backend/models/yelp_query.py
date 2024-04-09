7902fd8add68a28776752a00b67ad8a97a864c1b
Uche Okechukwu <uche.okechukwu@gmail.com>
2024-04-09 11:15:50 -0600

    completed conversational chain frontend

--- backend/models/yelp_query.py
+++ backend/models/yelp_query.py
@@ -1,6 +1,9 @@
 from pydantic import BaseModel
 class YelpQueryInput(BaseModel):
-    text: str
+    input: str
+    chat_history : list[str] = []
+# class YelpQueryInput(BaseModel):
+#     text: str
 class YelpQueryOutput(BaseModel):
     input: str
     output: str
--- main.py
+++ main.py
@@ -31,6 +31,8 @@ st.info(
 )
 if "messages" not in st.session_state:
     st.session_state.messages = []
+if "chat_history" not in st.session_state:
+    st.session_state.chat_history = []    
     
 for message in st.session_state.messages:
     with st.chat_message(message['role']):
@@ -43,25 +45,28 @@ for message in st.session_state.messages:
 if prompt := st.chat_input("What do you want to know?"):
     st.chat_message("user").markdown(prompt)
     st.session_state.messages.append({"role":"user", "output":prompt})
-    data = {"text": prompt}
+    # data = {"text": prompt}
+    data = {"input": prompt, "chat_history":st.session_state.chat_history}
     
     with st.spinner("The 🤖 is thinking..."):
         response = requests.post(CHATBOT_URL, json=data)
         if response.status_code == 200:
             output_text = response.json()["output"]
-            explanation = response.json()["intermediate_steps"]
+            # explanation = response.json()["intermediate_steps"]
+            st.session_state.chat_history += ['Human: '+prompt+'\nAI: '+output_text+'\n']
         else:
             output_text = """An error occurred while processing your message.
             Please try again or rephrase your message."""
-            explantion = output_text
+            # explantion = output_text
     
     st.chat_message("assistant").markdown(output_text)
-    st.status("How was this generated", state="complete").info(explanation)
+    # st.status("How was this generated", state="complete").info(explanation)
     
     st.session_state.messages.append(
         {"role": "assistant",
         "output": output_text,
-        "explanation": explanation}
+        # "explanation": explanation
+    }
     )
     
     
